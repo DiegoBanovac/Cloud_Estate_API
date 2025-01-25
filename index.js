ec9f7623ed09257e45a2b446268d4a927549e300
DiegoBanovac <diego.banovac10@gmail.com>
2025-01-25 18:19:12 +0100

    drugi commit

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({origin: 'https://cloud-estate-api.onrender.com'}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
