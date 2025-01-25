845d729b2f54713e9eb04d7bc6ee8956e7419b71
DiegoBanovac <diego.banovac10@gmail.com>
2025-01-25 18:40:58 +0100

    treci

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mysql = require("mysql");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:9000/' }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
